8bc87b52a0c78c8f6bc66e5c1ac27f9996d7aa63
rajesh224kr <rajesh224kr@gmail.com>
2023-07-16 15:50:45 +0530

    add category

---
+++ category/models.py
@@ -0,0 +1,18 @@
+from distutils.command.upload import upload
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+         category_name = models.CharField(max_length=50, unique=True)
+         slug = models.SlugField(max_length=50, unique=True)
+         description = models.TextField()
+         category_image = models.ImageField(upload_to = 'photos/categories', blank =True)
+         
+         class Meta:
+                  verbose_name = 'category'
+                  verbose_name_plural = 'categories'
+
+         
+         def __str__(self):
+                  return self.category_name
